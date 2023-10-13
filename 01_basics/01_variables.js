00b70b2e8da044609b7bc1349120f1e469b582d5
Mr. Suyog <101085525+suyog-tighare@users.noreply.github.com>
2023-10-13 07:08:51 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144335
+let accountEmail = "jnfeenfsn@gmail.com"
+var accountPassword = "123445"
+accountCity = "Jaipur"
+
+// accountEmail = 2
+
+accountEmail = "suyog@gmail.com"
+accountPassword = "2732y7374"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("3+3"); // we're using node,js not browser
+
+console.log("3+3") // -> 3+3 ; 
+// code readability should be high
+
+console.log("Hitesh")
+
+let name = "hitesh" // -> hitesh   --- string
+let age = 18   // ---integer
+let isLoggedIn = false // --- boolean
+
+//      Primitive Datatypes
+//number => 2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value (null /= 0)
+//undefined =>
+// symbol => unique
+
+//          Non- Primitive Datatypes
+// object
+
+console.log(typeof "hitesh")   // -> string
+console.log(typeof null )   // -> object
+console.log(typeof undefined)   // -> undefined
+
+
+
+
