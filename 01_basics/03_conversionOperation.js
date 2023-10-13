b72b67c402cf93fb7d923259383f77b4e963f903
Mr. Suyog <101085525+suyog-tighare@users.noreply.github.com>
2023-10-13 12:18:08 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// ------ converting datatypes into number -------
+
+//let score = 33
+
+//console.log(typeof  score);
+//console.log(typeof  (score));
+
+//let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1 , false => 0
+
+// ------ converting datatypes into boolean -------
+
+// let isLoggedIn =1
+
+// let boolenIsLoggedIn = Boolean (isLoggedIn)
+// console.log(boolenIsLoggedIn);
+
+// 1 => true , 0 =>  false
+//"" => false
+//"hitesh" => true
+
+// ------ converting datatypes into string -------
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof(stringNumber));
+console.log(stringNumber);
