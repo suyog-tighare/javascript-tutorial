99401986003a203d737fbc5dae0f6164c491c90d
Mr. Suyog <101085525+suyog-tighare@users.noreply.github.com>
2023-10-14 16:35:04 +0000

    datatype summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,9 +27,47 @@
 
 // ------ converting datatypes into string -------
 
-let someNumber = 33
+//let someNumber = 33
 
-let stringNumber = String(someNumber)
+//let stringNumber = String(someNumber)
 
-console.log(typeof(stringNumber));
-console.log(stringNumber);
+//console.log(typeof(stringNumber));
+//console.log(stringNumber);
+
+// *************Operations *********8
+
+// let value = 8
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+// let str3 = str1 + str2
+// console.log(str3)
+    
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(3+4*5%3);
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+ 
+// let num1,num2,num3
+
+// num1 = num2 =num3 = 2+2
+
+// let gameCounter = 100
+// gameCounter++ ;
+// console.log(gameCounter);
+
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// // ****Primitive**** (copy by Value type)
+
+// // 7 types : String, Number , Boolean , Null, undefined , Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail ;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+// const bigNumber = 32646343762874238472n
+
+// // ****Non-Primitive*** (Reference Type)
+
+// // Arrays , Objects, Functions
+
+// const heroes = ["shaktiman","naagraj","doga"];
+// let myObj = {
+//     name : "hitesh",
+//     age: 22,
+// }
+
+// const myFunction= function (){
+//    console.log("hello world");
+// }
+
+
+// console.log(typeof anotherId);
+// console.log(typeof myObj);
+// console.log(typeof heroes);
+// console.log(typeof myFunction);
+// console.log(typeof(bigNumber));
+// console.log(typeof score);
