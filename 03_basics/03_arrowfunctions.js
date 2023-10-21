b0d4a44bf515fabf297dbccde8d6b23a459dd9b2
Mr. Suyog <101085525+suyog-tighare@users.noreply.github.com>
2023-10-21 06:08:26 +0000

    ARROW START

---
+++ 03_basics/03_arrowfunctions.js
@@ -0,0 +1,30 @@
+// const user = {
+//     username : "hitesh",
+//     price : 999,
+//     welcomeMessage : function(){
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+//     }
+// }
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+function chai (){
+    let username = "hitesh";
+    console.log(this.username);
+}
+chai()
+
+const chai = function (){
+    let username = "hitesh"
+    console.log(this.username);
+}
+chai()
+
+const chai = () =>{
+    let username = "hitesh"
+    console.log(this);
+}
+chai(
+    
