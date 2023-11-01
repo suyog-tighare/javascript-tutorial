de470117572aec5af9f4126db0c95a16a7f673a7
Mr. Suyog <101085525+suyog-tighare@users.noreply.github.com>
2023-11-01 19:11:00 +0000

    arrow iife

--- 03_basics/03_arrowfunctions.js
+++ 03_basics/03_arrowfunctions.js
@@ -10,21 +10,58 @@
 // user.username = "sam"
 // user.welcomeMessage()
 
-function chai (){
-    let username = "hitesh";
-    console.log(this.username);
-}
-chai()
+// function chai (){
+//     let username = "hitesh";
+//     console.log(this.username);
+// }
+// chai()
 
-const chai = function (){
-    let username = "hitesh"
-    console.log(this.username);
-}
-chai()
+// const chai = function (){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+// chai()
 
-const chai = () =>{
-    let username = "hitesh"
-    console.log(this);
-}
-chai(
+// const chai = () =>{
+//     let username = "hitesh"
+//     console.log(this);
+// }
+// chai()
     
+// const addTwo = (num1,num2 ) => {
+// const addTwo = (num1,num2 ) => ({username:"hitesh"})
+    // return num1+num2
+// }
+
+// console.log(addTwo(3,4));
+
+// const myArray = [2,3,6,2,8] 
+// myArray.forEach()
+
+// Immediately Invoked Function Expression(IIFE)
+ //Syntax
+
+//  (function () {
+//     // …
+//   })();
+  
+//   (() => {
+//     // …
+//   })();
+  
+//   (async () => {
+//     // …
+//   })();
+
+  ///////////////////// apply //////
+
+// (function chai (){
+//     console.log(`DB Connected`);
+// }) ();
+
+( (name)=>{
+console.log(`DB Connected 2 ${name}`);
+}) ('hitesh')
+
+
+  
