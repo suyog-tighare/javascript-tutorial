d75efecad624f6fb023318f2a26c742814883b8a
Mr. Suyog <101085525+suyog-tighare@users.noreply.github.com>
2023-11-04 06:02:15 +0000

    loops 2

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,34 +1,43 @@
-// const userEmail = "h@hitest.com"
-// if(userEmail){
-//     console.log("got user email");
-// } else {
-//     console.log("dont have user email");
-// }
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
 
 // falsy values
-// false, 0 , -0 ,  BigInt 0n, "", null, undefined, NaN
 
-// truthy values
-// true , 1,"0", 'false' , " ", [],{}, function(){}, 
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
 
-// if email got is in array []
-// if (userEmail.length ===0){
-//     console.log("array is empty");
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
 // }
 
-// if email got is empty obj {}
-// const emptyObj = {}
-// if (Object.keys(emptyObj).length===0){
-// console.log("Object is empty");
-// }
+const emptyObj = {}
 
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
 
 // Nullish Coalescing Operator (??): null undefined
 
-let val1 ;
+let val1;
 // val1 = 5 ?? 10
 // val1 = null ?? 10
-// var1 = undefined ?? 15
+// val1 = undefined ?? 15
 val1 = null ?? 10 ?? 20
+
+
+
 console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
