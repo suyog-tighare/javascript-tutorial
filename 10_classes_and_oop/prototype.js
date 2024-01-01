89f5d14271e1e79a87f39ce3780c56a7605154b5
Mr. Suyog <101085525+suyog-tighare@users.noreply.github.com>
2024-01-01 17:44:23 +0000

    hmm

---
+++ 10_classes_and_oop/prototype.js
@@ -0,0 +1,18 @@
+// let myName = "hitesh     "
+// let myChannel = "chai"
+
+// console.log(myName.trim().length); -->low iq wala method
+
+
+let myHeros = ["thor","spiderman"]
+
+let heroPower ={
+    thor : "hammer",
+    spiderman : "sling",
+
+    getSpiderPower = function(){
+        console.log(`Spidey power is ${this.spiderman}`);
+    }
+}
+
+heroPower.hitesh()
